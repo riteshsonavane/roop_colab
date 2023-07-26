0e9dcf3f1c9e742499c7e1df05cf098695cae89f
Yifei <ryf123@gmail.com>
2023-07-26 09:31:47 -0700

    in memory processing frames

--- debug.py
+++ debug.py
@@ -25,6 +25,29 @@ def find_source_face_and_target_distances(target_faces, reference_face):
                 distances.append(np.sum(np.square(face.normed_embedding - reference_face.normed_embedding)))
     return distances
 
+def load_video_in_memoery(video_path):
+    cap = cv2.VideoCapture(video_path)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    print(f"Total frames: {total_frames}")
+
+    frame_count = 0
+    frames = []
+    while True:
+        # Read frame from video
+        ret, frame = cap.read()
+        frames.append(frame)
+        # If frame is read successfully, save it
+        if ret:
+            cv2.imwrite(frame_filename, frame)
+            frame_count += 1
+        else:
+            break
+
+    # Release the video capture object
+    cap.release()
+    return frames
+
 if __name__ == '__main__':
     providers = ['CPUExecutionProvider']
     app = FaceAnalysis(providers=providers, name='buffalo_l')
